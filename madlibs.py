45c6b76e9fa6de4677d200dd6c0a145faeb4480b
habinpark <83361825+habinpark@users.noreply.github.com>
2022-03-07 16:48:16 -0500

    Added madlib files both html and py

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+userInputNoun = input('Enter a Noun: ')
+userInputVerb = input('Enter a Verb: ')
+userInputAdjective = input('Enter a Adjective: ')
+userInputAdverb = input('Enter a Adverb:' )
+
+print("On a tropical " + userInputNoun + "," + " Underneath a " + userInputAdjective + " moon. " + userInputVerb + " with the hula dancers, " + userInputAdverb + " Askin' questions cause' they got all the answers.")
