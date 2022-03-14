186002f907906e1013887424b37e40ec85d1e7b6
habinpark <83361825+habinpark@users.noreply.github.com>
2022-03-14 15:09:31 -0400

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -3,4 +3,7 @@ userInputVerb = input('Enter a Verb: ')
 userInputAdjective = input('Enter a Adjective: ')
 userInputAdverb = input('Enter a Adverb:' )
 
-print("On a tropical " + userInputNoun + "," + " Underneath a " + userInputAdjective + " moon. " + userInputVerb + " with the hula dancers, " + userInputAdverb + " Askin' questions cause' they got all the answers.")
+print("On a tropical " + userInputNoun + ",")
+print("Underneath a " + userInputAdjective + " moon. " )
+print(userInputVerb + " with the hula dancers," )
+print(userInputAdverb + " askin' questions cause' they got all the answers.")
